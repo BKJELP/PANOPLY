55412c5b2187acb01fa8939990ea98d3e9a0e801
Brice Levasseur <brice.k.levasseur@gmail.com>
2021-04-24 17:58:32 +0200

    add outfit controller

---
+++ app/controllers/outfits_controller.rb
@@ -0,0 +1,43 @@
+class OutfitsController < ApplicationController
+    def index
+        @outfits = Outfit.all
+    end
+
+    def show
+        @outfits = Outfit.find(params[:id])
+    end
+
+    def new
+        @outfits = Outfit.new
+    end
+
+    def create 
+        @outifs = Outfit.new(outfit_params)
+        if @outfit.save
+            redirect_to outfit_path(@outfit)
+        else
+            render "new"
+        end
+    end
+
+    def list
+        @outfits = Outfit.find(params[:user_id])
+    end
+
+    def update
+        @outfits = Outfit.find(params[:id])
+        @outfit.update(outfit_params)
+        redirect_to outfit_path(@outfit)
+    end
+
+    def destroy
+        @outfit = Outfit.find(params[id])
+        @outfit.destroy
+        redirect_to outfit_path
+    end
+
+private
+
+    def outfit_params
+        params.require(:outfits).permit(:name, :description, :price, :image, :category, :user_id)
+    end
