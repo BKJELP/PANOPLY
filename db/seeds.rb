30093a090a0b03c6201cfa957158e362d60c44e8
guillaume fraud <guillaume.fraud.kedgebs@gmail.com>
2021-04-29 20:55:19 +0200

    eee

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'We delete the old data'
+User.destroy_all
+Outfit.destroy_all
 puts 'Creating admin...'
 
 guillaume = User.new(first_name: "Guillaume", last_name: "Fraud", password: "123456", email: "guillaume@rentease.com")
@@ -18,9 +21,12 @@ adrien.save!
 puts 'Finished!'
 
 puts 'Creating outfits...'
-tech_interview = Outfit.new(name:"Let's shine as a baby unicorn", description:"You cannot join a unicorn without looking like a unicorn. As simple as that. 100 percent success rate", price:"35", catagory:"startup")
+tech_interview = Outfit.new(name:"Let's shine as a baby unicorn", description:"You cannot join a unicorn without looking like a unicorn. As simple as that. 100 percent success rate", price:"35", category:"startup")
+tech_interview.user = guillaume
 ma_interview = Outfit.new(name:"Become a Wolf of Wall Street", description:"Was Leonardo DiCaprio a professional banker? No but at least he wore the uniform. There is no other factor of success", price:"10000",category:"corporate")
+ma_interview.user = brice
 teacher_interview = Outfit.new(name:"How to be a magic teacher at Poudlard", description:"Albus Dumbledore has only a uniform and no talent. Be like Albus, become a wizard master.", price: "56", category:"public")
+teacher_interview.user = adrien
 tech_interview.save!
 ma_interview.save!
 teacher_interview.save!
