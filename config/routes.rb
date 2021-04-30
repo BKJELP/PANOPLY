63758ae78952eef4cde46853362635db87dce9de
William <allinwilliam@gmail.com>
2021-04-30 14:51:33 +0200

    Des petits soucis avec ActiveStorage et les seeds, je laisse :image pour l'instant en seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
-  resources :users, only: [:new, :create] do
-    resources :outfits, only: [:list, :new, :create, :update, :show]
-  end
+  resources :outfits, only: [:list, :index, :new, :create, :update, :show]
   resources :outfits, only: [ :destroy ]
-  resources :outfits, only: [ :index, :show] do
-    resources :reservations, only: [:new, :create, :index, :update]
-  end
+  resources :reservations, only: [:new, :create, :index, :update]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
