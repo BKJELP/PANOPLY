ee2a2a9e214331c97b9ba124203fa892b5f44d00
William <allinwilliam@gmail.com>
2021-04-30 17:15:12 +0200

    Pas fini

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
-  resources :users, only: [:new, :create] do
-    resources :outfits, only: [:list, :new, :create, :update, :show]
-  end
-  resources :outfits, only: [ :destroy ]
+  resources :outfits, only: [:list, :new, :create, :update, :show, :destroy]
   resources :outfits, only: [ :index, :show] do
     resources :reservations, only: [:new, :create, :index, :update]
   end
