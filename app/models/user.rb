41fa764f6e921257f69902dc98ffda6c66a5fd9b
William <allinwilliam@gmail.com>
2021-05-01 14:36:33 +0200

    fixed seeds

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :reservations
-  has_many :outfits
+  has_many :outfits, :dependent => :delete_all
 end
