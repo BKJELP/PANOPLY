4b8e0a869a0e46b0c16e45e36b55d051c23bc4f2
Brice Levasseur <brice.k.levasseur@gmail.com>
2021-05-04 21:48:23 +0200

    save 4 may 2148

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2021_05_01_095615) do
     t.text "description"
     t.integer "price"
     t.text "category"
+    t.text "image"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
